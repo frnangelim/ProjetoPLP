49ba93bcf6f322771002a70557a971577dbf0a0f
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-07-21 22:32:21 -0300

    Refatorações interessantes, principalmente na preparação da mensagem

--- CifrarMensagem.hs
+++ CifrarMensagem.hs
@@ -11,61 +11,62 @@ _TABELA = ([['Y','Q','D','L','G'],
 
 main :: IO ()
 main = do
-  mostrarMenuDeOpcoes
-  
-  opcaoArgs <- getLine
-  let opcao = read opcaoArgs
-  
-  mensagemArgs <- getLine
-  let mensagem = read mensagemArgs
-  
-  let mensagemPreparada = preparaMensagem mensagem
-  let resultadoProcessamento = processa opcao mensagemPreparada
-  
-  putStrLn ("\n")
-  
-  if not (ehVazia resultadoProcessamento) then do
-	{ putStrLn (resultadoProcessamento ++ "\n"); main }
-  else do
-    { putStrLn ("Volte sempre! :)") }
+	mostrarMenuDeOpcoes
+
+	opcaoArgs <- getLine
+	let opcao = read opcaoArgs
+
+	mensagemArgs <- getLine
+	let mensagem = read mensagemArgs
+	
+	let resultadoProcessamento = processa opcao mensagem
+
+	if not (ehVazia resultadoProcessamento) then do
+		{ putStrLn (resultadoProcessamento ++ "\n"); main }
+	else do
+		{ putStrLn ("Volte sempre! :)") }
+
+mostrarMenuDeOpcoes :: IO()
+mostrarMenuDeOpcoes = do
+	putStrLn ("Escolha uma das opcoes abaixo:\n")
+	putStrLn ("1. Cifrar mensagem")
+	putStrLn ("2. Ver o alfabeto")
+	putStrLn ("3. Terminar\n")
+	putStrLn ("Opcao: ")
+
+processa :: Int -> String -> String
+processa 1 mensagem = "\n" ++ adicionaEspacoACada 4 (cifraMensagem (preparaMensagem mensagem))
+processa 2 mensagem = transformaTabela _TABELA
+processa 3 mensagem = ""
+processa opcaoInvalida mensagem = "Você inseriu uma opção inválida, que tal tentar novamente?"
 
 preparaMensagem :: String -> String
-preparaMensagem frase = juntaDoisADois fraseValida
+preparaMensagem frase = map toUpper (juntaDoisADois fraseValida)
     where fraseValida = removeCaracInvalidos frase
 
 juntaDoisADois :: String -> String
 juntaDoisADois [] = ""
 juntaDoisADois (x:[]) = (x:'X':[])
 juntaDoisADois (x:y:xs) = if x == y then (x:'X':[]) ++ juntaDoisADois (y:xs)
-						  else(x:y:[]) ++ juntaDoisADois xs
+						  else (x:y:[]) ++ juntaDoisADois xs
 
 removeCaracInvalidos :: String -> String
 removeCaracInvalidos frase = [ x | x <- frase, isLetter x ]
 
-mostrarMenuDeOpcoes :: IO()
-mostrarMenuDeOpcoes = do
-  putStrLn ("Escolha uma das opcoes abaixo:\n")
-  putStrLn ("1. Cifrar mensagem")
-  putStrLn ("2. Ver o alfabeto")
-  putStrLn ("3. Terminar\n")
-  putStrLn ("Opcao: ")
-
-processa :: Int -> String -> String
-processa 1 mensagem = adicionaEspacoCadaQuatro (cifraMensagem mensagem)
-processa 2 mensagem = transformaTabela _TABELA
-processa opcaoInvalida mensagem = ""
-
 transformaTabela :: [[Char]] -> String
 transformaTabela [] = ""
-transformaTabela (x:xs) = adicionaEspacoEntreCaracters x ++ "\n" ++ transformaTabela xs
+transformaTabela (x:xs) = (adicionaEspacoACada 1 x) ++ "\n" ++ transformaTabela xs
 
 adicionaEspacoEntreCaracters :: String -> String
 adicionaEspacoEntreCaracters [] = ""
 adicionaEspacoEntreCaracters (x:xs) = x:' ':[] ++ adicionaEspacoEntreCaracters xs
 
+adicionaEspacoACada :: Int -> String -> String
+adicionaEspacoACada fatorPulo frase = if length frase <= fatorPulo then frase
+								 else (take fatorPulo frase) ++ " " ++ adicionaEspacoACada fatorPulo (drop fatorPulo frase)
+
 ehVazia :: String -> Bool
-ehVazia "" = True
-ehVazia string = False
+ehVazia string = string == ""
 
 estaNaMesmaLinha :: Char -> Char -> Bool
 estaNaMesmaLinha primeiraLetra segundaLetra = indiceLinhaPrimeiraLetra == indiceLinhaSegundaLetra
@@ -90,21 +91,22 @@ getColunaNaMatriz letra indiceLinha indiceColuna = if letra == letraDaColunaAtua
     where letraDaColunaAtual = _TABELA !! indiceLinha !! indiceColuna
 
 letraDaDireita :: Char -> Char
-letraDaDireita letra = let linha = getLinhaNaMatriz letra _INDICE_TABELA in let coluna = getColunaNaMatriz letra linha _INDICE_TABELA in
-                              _TABELA !! linha !! ((coluna+1) `mod` _TAMANHO_TABELA)
+letraDaDireita letra = let linha = getLinhaNaMatriz letra _INDICE_TABELA in 
+					   let colunaAtual = getColunaNaMatriz letra linha _INDICE_TABELA in
+					   let colunaAoLado = (colunaAtual+1) `mod` _TAMANHO_TABELA in
+                       _TABELA !! linha !! colunaAoLado
 
 letraDeBaixo :: Char -> Char
-letraDeBaixo letra = let linha = getLinhaNaMatriz letra _INDICE_TABELA in let coluna = getColunaNaMatriz letra linha _INDICE_TABELA in
-                            _TABELA !! ((linha+1) `mod` _TAMANHO_TABELA) !! coluna
+letraDeBaixo letra = let linhaAtual = getLinhaNaMatriz letra _INDICE_TABELA in
+                     let linhaAbaixo = (linhaAtual+1) `mod` _TAMANHO_TABELA in
+					 let coluna = getColunaNaMatriz letra linhaAtual _INDICE_TABELA in
+                     _TABELA !! linhaAbaixo !! coluna
 
 correspondente :: Char -> Char -> Char
-correspondente primeiraLetra segundaLetra = let linha = getLinhaNaMatriz primeiraLetra _INDICE_TABELA in 
-                                                   let coluna = getColunaNaMatriz segundaLetra (getLinhaNaMatriz segundaLetra _INDICE_TABELA) _INDICE_TABELA in
-                                                   _TABELA !! linha !! coluna
-
-adicionaEspacoCadaQuatro :: String -> String
-adicionaEspacoCadaQuatro frase = if length frase <= 4 then frase
-							  else take 4 frase ++ " " ++ adicionaEspacoCadaQuatro (drop 4 frase)
+correspondente primeiraLetra segundaLetra = let linhaPrimeiraLetra = getLinhaNaMatriz primeiraLetra _INDICE_TABELA in 
+											let linhaSegundaLetra = getLinhaNaMatriz segundaLetra _INDICE_TABELA in
+										    let coluna = getColunaNaMatriz segundaLetra linhaSegundaLetra _INDICE_TABELA in
+										    _TABELA !! linhaPrimeiraLetra !! coluna
 
 cifraMensagem :: String -> String
 cifraMensagem [] = []
