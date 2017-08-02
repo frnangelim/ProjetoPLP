0bdb5eb432c709d5838f3038963e36b1c103026f
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-08-02 19:06:06 -0300

    Começando a parte de prolog

---
+++ CifrarMensagem.pl
@@ -0,0 +1,10 @@
+
+
+TABELA = ([['Y','Q','D','L','G'],
+			['M','J','X','F','U'],
+			['V','W','C','P','B'],
+			['O','S','K','R','E'],
+			['T','H','N','A','I']])
+			
+INDICE_TABELA is 4
+TAMANHO_TABELA is 5
