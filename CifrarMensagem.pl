52508440de2c47e4afbd6a0f2e6734a60c6ac3dd
Francisco Angelim <frnangelim@gmail.com>
2017-08-22 17:24:27 -0300

    Done.

--- CifrarMensagem.pl
+++ CifrarMensagem.pl
@@ -14,15 +14,15 @@ encontraLinha(E,[_|T],I,R) :- IX is I+1, encontraLinha(E,T,IX,R).
 encontraColuna(E,[E|_],I,I).
 encontraColuna(E,[_|T],I,R) :- IX is I+1, encontraColuna(E,T,IX,R).
 
-estaNaMesmaLinha(E,E1) :- matriz(_,M), encontraLinha(E,M,0,I),encontraLinha(E1,M,0,I1), I = I1.
-estaNaMesmaColuna(E,E1) :- matriz(_,M), encontraLinha(E,M,0,I),encontraLinha(E1,M,0,I1), 
+estaNaMesmaLinha(E,M,E1) :- encontraLinha(E,M,0,I),encontraLinha(E1,M,0,I1), I = I1.
+estaNaMesmaColuna(E,M,E1) :- encontraLinha(E,M,0,I),encontraLinha(E1,M,0,I1), 
 							getByIndice(I,M,0,L1),getByIndice(I1,M,0,L2),
 							encontraColuna(E,L1,0,J),encontraColuna(E1,L2,0,J1), J = J1.
 
-letraDeBaixo(E,R) :- matriz(_,M),encontraLinha(E,M,0,I),getByIndice(I,M,0,L),encontraColuna(E,L,0,J),IX is mod(I+1,5), getByIndice(IX,M,0,LX), getByIndice(J,LX,0,R).
-letraDaDireita(E,R) :- matriz(_,M),encontraLinha(E,M,0,I),getByIndice(I,M,0,L),encontraColuna(E,L,0,J),IJ is mod(J+1,5), getByIndice(IJ,L,0,R).
+letraDeBaixo(E,M,R) :- encontraLinha(E,M,0,I),getByIndice(I,M,0,L),encontraColuna(E,L,0,J),IX is mod(I+1,5), getByIndice(IX,M,0,LX), getByIndice(J,LX,0,R).
+letraDaDireita(E,M,R) :- encontraLinha(E,M,0,I),getByIndice(I,M,0,L),encontraColuna(E,L,0,J),IJ is mod(J+1,5), getByIndice(IJ,L,0,R).
 
-correspondente(E,E1,R,R1) :- matriz(_,M),encontraLinha(E,M,0,I),encontraLinha(E1,M,0,I1),
+correspondente(E,E1,M,R,R1) :- encontraLinha(E,M,0,I),encontraLinha(E1,M,0,I1),
 								getByIndice(I,M,0,L1),getByIndice(I1,M,0,L2),
 								encontraColuna(E,L1,0,J1),encontraColuna(E1,L2,0,J2),
 								getByIndice(J2,L1,0,R), getByIndice(J1,L2,0,R1).
@@ -30,21 +30,31 @@ correspondente(E,E1,R,R1) :- matriz(_,M),encontraLinha(E,M,0,I),encontraLinha(E1
 getByIndice(I,[E|_],I, E).
 getByIndice(I,[_|L],IA, R) :- getByIndice(I,L,IB,R), IA is IB-1.
 
-cifraMensagem([], '').
-cifraMensagem([A,B|C],R):- estaNaMesmaLinha(A,B), letraDaDireita(A,AD), letraDaDireita(B,BD), string_concat(AD,BD,X), cifraMensagem(C, R1), string_concat(X,R1, R2), R = R2.
-cifraMensagem([A,B|C],R):- estaNaMesmaColuna(A,B),letraDeBaixo(A,AB), letraDeBaixo(B,BB), string_concat(AB,BB,X), cifraMensagem(C, R1), string_concat(X,R1, R2), R = R2.
-cifraMensagem([A,B|C],R):- correspondente(A,B,AC,BC), string_concat(AC,BC,X), cifraMensagem(C, R1), string_concat(X,R1, R2), R = R2.
+cifraMensagem([], _,'').
+cifraMensagem([A,B|C],M,R):- estaNaMesmaLinha(A,M,B), letraDaDireita(A,M,AD), letraDaDireita(B,M,BD), string_concat(AD,BD,X), cifraMensagem(C,M, R1), string_concat(X,R1, R2), R = R2.
+cifraMensagem([A,B|C],M,R):- estaNaMesmaColuna(A,M,B),letraDeBaixo(A,M,AB), letraDeBaixo(B,M,BB), string_concat(AB,BB,X), cifraMensagem(C, M,R1), string_concat(X,R1, R2), R = R2.
+cifraMensagem([A,B|C],M,R):- correspondente(A,B,M,AC,BC), string_concat(AC,BC,X), cifraMensagem(C,M, R1), string_concat(X,R1, R2), R = R2.
 
-cabeca([H|T], H).
-cauda([H|T], T).
+quatroAquatro([], []).
+quatroAquatro([A,B|[]], R) :- R = [A,B].
+quatroAquatro([A,B,C,D|[]], R) :- R = [A,B,C,D].
+quatroAquatro([A,B,C,D|T], R) :- append([A,B,C,D], [' '], R1), quatroAquatro(T, R2), append(R1, R2, R).
+
+cabeca([H|_], H).
+cauda([_|T], T).
 
 colocaX(Lista, [], R) :- append([Lista], ['X'], LX), R = LX.
 colocaX(Lista, [Lista|Cauda], [Lista|['X'|S]]) :- colocaX(Lista,Cauda,S).
 colocaX(Lista, [Cabeca|Cauda], [Lista|S]) :- colocaX(Cabeca,Cauda,S).
 
-retiraEspaco(L, [] , []).
+retiraEspaco(_, [] , []).
 retiraEspaco(L, [L|T], G):- retiraEspaco(L, T, G).
 retiraEspaco(L, [H|T] , [H|G]):- retiraEspaco(L, T, G).
+
+removeInvalidos([], []).
+removeInvalidos([H|T], FraseValida) :- is_alpha(H), removeInvalidos(T, R), append([H], R, R1), FraseValida = R1.
+removeInvalidos([H|T], FraseValida) :- \+ is_alpha(H), removeInvalidos(T, R), FraseValida = R.
+
 
 /*Funcaoo que retorna a matriz atual*/
 retornaMatriz:-
@@ -69,16 +79,38 @@ encerra:-
 	write("Fim da execucao!"),nl,nl,halt(0).
 
 
+
 matriz(_,[['Y','Q','D','L','G'],
 		['M','J','X','F','U'], 
 		['V','W','C','P','B'], 
 		['O','S','K','R','E'], 
 		['T','H','N','A','I']]).
 
-execute(1) :- read(X),write(X),main. /* Preparar e cifrar mensagem */
-execute(2) :- matriz(_,M), escreveMatriz(M),main.
-execute(3) :- matriz(_,M), matrizAleatoria(M,M1), escreveMatriz(M1),main.
-execute(4) :- encerra,main.
+execute(1,M) :- 
+	read(M2),
+	string_chars(M2,Mensagem1),
+	removeInvalidos(Mensagem1, Mensagem),
+	retiraEspaco(' ', Mensagem, MensagemSemEspaco),
+	cauda(MensagemSemEspaco, AuxCauda),
+	cabeca(MensagemSemEspaco, AuxCabeca),
+	colocaX(AuxCabeca, AuxCauda, MensagemX),
+	cifraMensagem(MensagemX,M,R), string_chars(R,RL),quatroAquatro(RL,RF),atomic_list_concat(RF,RFF),write(RFF),nl, main2(M).
+
+execute(2,M) :- escreveMatriz(M),main2(M).
+execute(3,M) :- matrizAleatoria(M,M1), escreveMatriz(M1),main2(M1).
+execute(4,_) :- encerra,main.
+
+
+main2(M):-
+	write("Digite o numero da opcao:"),nl,
+	write("1. Introduzir uma mensagem para cifrar"),nl,
+	write("2. Ver o alfabeto"),nl,
+	write("3. Escolher uma tabela de cifra nova"),nl,
+	write("4. Terminar\n" ),nl,
+
+	read(L),
+	execute(L,M),nl.
+
 
 :- initialization main.
 
@@ -88,17 +120,9 @@ main:-
 	write("2. Ver o alfabeto"),nl,
 	write("3. Escolher uma tabela de cifra nova"),nl,
 	write("4. Terminar\n" ),nl,
-	
-	read_line_to_codes(user_input, M),
-	string_to_atom(M,M2),
-	string_chars(M2,Mensagem),
-	
-	retiraEspaco(' ', Mensagem, MensagemSemEspaco),
-	
-	cauda(MensagemSemEspaco, AuxCauda),
-	cabeca(MensagemSemEspaco, AuxCabeca),
-	
-	colocaX(AuxCabeca, AuxCauda, MensagemX),
-	write(MensagemX), nl.
+	matriz(_,M),
+
+	read(L),
+	execute(L,M),nl.
 	
 	
